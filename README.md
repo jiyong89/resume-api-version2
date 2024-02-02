10eeb8e9b5ec939b6299917a3d8fcbc3ae9eb601
jiyongSong <jiyongsong89@gmail.com>
2024-02-02 23:51:29 +0900

    router 분리

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # resume-api
+
+<h3>ERD IMAGE
+<img src ="ERD.png">
+
+Api Specification Link : https://jiyongsong.notion.site/resume-api-5ea6c66def0f49d1b2fa527a177fd280?pvs=4
--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const userRouter = require('./routers/user.router')
+const resumeRouter = require('./routers/resume.router')
+
 const app = express()
-const port = 3000
+const port = 3306
+
+app.use(bodyParser.json()) //미들웨어 등록
+
+app.use('/users', userRouter)
+app.use('/resumes', resumeRouter);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
