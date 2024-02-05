56c4753275dea504f3dd6fca9097ca50cc827954
jiyongSong <jiyongsong89@gmail.com>
2024-02-05 22:38:42 +0900

    resume api

--- routers/resumes.router.js
+++ routers/resumes.router.js
@@ -1,9 +1,51 @@
-import express from 'express'
-const router = express.Router()
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+import jwt from 'jsonwebtoken';
+import jwtvalidate from '../middleware/jwt-validate.middleware.js';
 
-router.get('/', (req,res)=> {
-    res.send("resume router!")
-})
+const prisma = new PrismaClient();
 
+const router = express.Router();
 
-export default router
+router.get('/', async (req, res) => {
+  const orderkey = req.query.orderkey ?? 'resumeId';
+  const orderValue = req.query.orderValue ?? 'desc';
+
+  if (!['resumeId', 'status'].includes(orderkey)) {
+    return res.status(400).json({
+      success: false,
+      message: 'orderKey가 올바르지 않습니다.',
+    });
+  }
+  if (!['asc', 'desc'].includes(orderValue.toLowerCase())) {
+    return res.status(400).json({
+      success: false,
+      message: 'orderKey가 올바르지 않습니다.',
+    });
+  }
+
+  const resumes = await prisma.resumes.findMany({
+    select: {
+      resumeId: true,
+      title: true,
+      content: true,
+      user: {
+        select: {
+          name: true,
+        },
+      },
+      createdAt: true,
+    },
+    orderBy: {
+      [orderkey]: orderValue.toLowerCase(),
+    },
+  });
+  resumes.forEach((resume) => {
+    resume.name = resume.user.name;
+    delete resume.user;
+  });
+
+  return res.json({ data: resumes });
+});
+
+export default router;
